f3ef2c2118b41f7bb70f72b5a44c514d9cdbfd58
U-DESKTOP-JLB2FSD\cxue1 <tsit20@stuy.edu>
2021-10-05 21:50:35 -0400

    finished hw

--- mystring.c
+++ mystring.c
@@ -9,6 +9,83 @@ int mystrlen(char *s){
 }
 
 char * mystrcpy(char *dest, char *source){
-	
+	int i = 0;
+	while (*dest!=0 || *source!=0){
+		i++;
+		dest++;
+		source++;
+	}
+	while (i){
+		*dest = *source;
+		dest--;
+		source--;
+		i--;
+	}
+	*dest = *source;
+	return dest;
 }
 
+char * mystrncpy(char *dest, char *source, int n){
+	int k = n;
+	int j = 0;
+	while (n){
+		if (!*dest){
+			return dest-j;
+		}
+		if (*source){
+			*dest = *source;
+		}
+		else{
+			return dest-j;
+		}
+		dest++;
+		source++;
+		n--;
+		j++;
+	}
+	return dest-k;
+}
+
+char * mystrcat(char *dest, char *source){
+	int dest_len = mystrlen(dest);
+	int source_len = mystrlen(source);
+	int copy = source_len;
+	dest+=dest_len;
+	while (source_len){
+		*dest = *source;
+		dest++;
+		source++;
+		source_len--;
+	}
+	*dest = 0;
+	dest-=(dest_len+copy);
+	return dest;
+}
+
+int mystrcmp(char *s1, char *s2){
+	while(*s1 == *s2){
+		if (*s1){
+			s1++;
+			s2++;
+		}
+		else{
+			return 0;
+		}
+	}
+	if (*s1 > *s2){
+		return 1;
+	}
+	return -1;
+}
+
+char * mystrchr(char *s, char c){
+	int i = mystrlen(s);
+	while (*s!=c){
+		s++;
+		i--;
+		if (i==0 && c!=0){
+			return 0;
+		}
+	}
+	return s;
+}
